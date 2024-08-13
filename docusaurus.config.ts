f09347855934d77f597437f29fc53aecf60de787
Gillian97 <jinling.sunshine@gmail.com>
2024-08-13 22:57:12 +0800

    fix: path

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: "https://Gillian97.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/blog-website/",
   trailingSlash: false,
   deploymentBranch: "gh-pages",
 
